42112dc729b8b7b55e6c56020e944871190c42b5
HLLauCode <97733869+HLLauCode@users.noreply.github.com>
2022-05-01 10:46:51 +0800

    removed dogs part

--- permissions/users.js
+++ permissions/users.js
@@ -38,34 +38,3 @@ exports.delete = (requester, data) =>
   ac.can(requester.role)
     .context({requester:requester.ID, owner:data.ID})
     .execute('delete').sync().on('user');
-
-
-ac.grant('admin').execute('read').on('dogs')
-ac.grant('admin').execute('create').on('dogs')
-ac.grant('admin').execute('update').on('dogs')
-ac.grant('admin').execute('delete').on('dogs')
-
-exports.readAll = (requester) => 
-  ac.can(requester.role).execute('read').sync().on('dogs')
-
-
-exports.read = (requester, data) => 
-  ac.can(requester.role)
-    .context({ requester:requester.ID, owner:data.ID})
-    .execute('read').sync().on('dogs')
-
-exports.create = (requester, data) => 
-  ac.can(requester.role)
-    .context({ requester:requester.ID, owner:data.ID})
-    .execute('create').sync().on('dogs')
-
-exports.update = (requester, data) => 
-  ac.can(requester.role)
-    .context({requester:requester.ID, owner:data.ID})
-    .execute('update').sync().on('dogs');
-
-
-exports.delete = (requester, data) => 
-  ac.can(requester.role)
-    .context({requester:requester.ID, owner:data.ID})
-    .execute('delete').sync().on('dogs');
