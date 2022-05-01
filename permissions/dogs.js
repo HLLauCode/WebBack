b2440856b466fc1399a60a545d8d6e1c3dceba6a
HLLauCode <97733869+HLLauCode@users.noreply.github.com>
2022-05-01 16:28:32 +0800

    updated some coding style

--- permissions/dogs.js
+++ permissions/dogs.js
@@ -4,30 +4,26 @@ const ac = new AccessControl()
 ac.grant('user').execute('read').on('dogs')
 
 ac.grant('admin').execute('read').on('dogs')
-ac.grant('admin').execute('create').on('dogs')
-ac.grant('admin').execute('update').on('dogs')
-ac.grant('admin').execute('delete').on('dogs')
+  .execute('create').on('dogs')
+  .execute('update').on('dogs')
+  .execute('delete').on('dogs')
 
 exports.readAll = (requester) => 
   ac.can(requester.role).execute('read').sync().on('dogs')
 
 exports.read = (requester, data) => 
   ac.can(requester.role)
-    .context({ requester:requester.ID, owner:data.ID})
     .execute('read').sync().on('dogs')
 
 exports.create = (requester, data) => 
   ac.can(requester.role)
-    .context({ requester:requester.ID, owner:data.ID})
     .execute('create').sync().on('dogs')
 
 exports.update = (requester, data) => 
   ac.can(requester.role)
-    .context({requester:requester.ID, owner:data.ID})
     .execute('update').sync().on('dogs');
 
 
 exports.delete = (requester, data) => 
   ac.can(requester.role)
-    .context({requester:requester.ID, owner:data.ID})
     .execute('delete').sync().on('dogs');
