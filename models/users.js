cdc70e81ca80f8b382a0ee3d9f70607d8ec506db
HLLauCode <97733869+HLLauCode@users.noreply.github.com>
2022-05-09 21:59:39 +0800

    Added create user function

--- models/users.js
+++ models/users.js
@@ -1,15 +1,31 @@
 const db = require('../helpers/database')
 
-exports.findByUsername = async function getByUsername(username) {
-  const query = "SELECT * FROM users WHERE username = ?"
-  let values = [username]
+exports.findByUsername = async function getByUsername (username) {
+  const query = 'SELECT * FROM users WHERE username = ?'
+  const values = [username]
   const user = await db.run_query(query, values)
   return user
 }
 
 exports.getAll = async function getAll (page, limit, order) {
   // TODO: use page, limit, order to give pagination
-  let query = "SELECT * FROM users;"
-  let data = await db.run_query(query)  
+  const query = 'SELECT * FROM users;'
+  const data = await db.run_query(query)
   return data
+}
+
+exports.add = async function add (user) {
+  let keys = Object.keys(user)
+  const values = Object.values(user)
+  keys = keys.join(',')
+  let parm = ''
+  for (i = 0; i < values.length; i++) { parm += '?,' }
+  parm = parm.slice(0, -1)
+  const query = `INSERT INTO users (${keys}) VALUES (${parm})`
+  try {
+    await db.run_insert(query, values)
+    return { status: 201 }
+  } catch (error) {
+    return { status: 400 , error }
+  }
 }
