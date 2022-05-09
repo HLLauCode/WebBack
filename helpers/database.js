f7f35e136284d295aba81f21dece675a934ab16c
HLLauCode <97733869+HLLauCode@users.noreply.github.com>
2022-05-09 21:58:41 +0800

    Update database.js

--- helpers/database.js
+++ helpers/database.js
@@ -1,63 +1,58 @@
-const {Sequelize, QueryTypes} = require('sequelize')  
+const { Sequelize, QueryTypes } = require('sequelize')
 const info = require('../config')
 
-// define an async utility function to get a connection
-// run an SQL query then end the connection
-exports.run_query = async function run_query(query, values) {
+exports.run_query = async function run_query (query, values) {
   try {
     const sequelize = new Sequelize(`postgres://${info.config.user}:${info.config.password}@${info.config.host}:${info.config.port}/${info.config.database}`)
     await sequelize.authenticate()
-    let data = await sequelize.query(query, { 
+    const data = await sequelize.query(query, {
       replacements: values,
-      type: QueryTypes.SELECT 
-    })    
+      type: QueryTypes.SELECT
+    })
     await sequelize.close()
     return data
   } catch (error) {
-    console.error(error, query, values);
-    throw 'Database query error'
+    console.error(error, query, values)
+    throw ('Database query error', error)
   }
 }
 
-exports.run_insert = async function run_insert(sql, values) {
+exports.run_insert = async function run_insert (sql, values) {
   try {
     const sequelize = new Sequelize(`postgres://${info.config.user}:${info.config.password}@${info.config.host}:${info.config.port}/${info.config.database}`)
     await sequelize.authenticate()
-    
-    console.log('start '+values)
-    
-    let data = await sequelize.query(sql, {
+
+    console.log('start ' + values)
+
+    const data = await sequelize.query(sql, {
       replacements: values,
       type: QueryTypes.INSERT
-    })    
+    })
     console.log('end')
     await sequelize.close()
     console.log('return')
     return data
   } catch (error) {
-    console.error(error, query, values);
-    throw 'Database query error'
+    console.error(error, query, values)
+    throw ('Database query error', error)
   }
 }
 
-exports.run_update = async function run_update(sql, values) {
+exports.run_update = async function run_update (sql, values) {
   try {
     const sequelize = new Sequelize(`postgres://${info.config.user}:${info.config.password}@${info.config.host}:${info.config.port}/${info.config.database}`)
     await sequelize.authenticate()
-    console.log('start updating '+values)
-
-    let data = await sequelize.query(sql, {
+    console.log('start updating ' + values)
+    const data = await sequelize.query(sql, {
       replacements: values,
       type: QueryTypes.UPDATE
-    })  
-    
+    })
     console.log('end')
     await sequelize.close()
     console.log('return')
     return data
-    
-  } catch {
-    console.error(error, query, values);
-    throw 'Database query error'
+  } catch (error) {
+    console.error(error, query, values)
+    throw ('Database query error', error)
   }
 }
