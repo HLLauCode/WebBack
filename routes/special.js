ded70e0faa9500059a51b97cbd092eb79115837c
HLLauCode <97733869+HLLauCode@users.noreply.github.com>
2022-05-09 22:07:34 +0800

    Update special.js

--- routes/special.js
+++ routes/special.js
@@ -2,22 +2,22 @@ const Router = require('koa-router')
 const bodyParser = require('koa-bodyparser')
 const auth = require('../controllers/auth')
 
-const router = Router({prefix: '/api/v1'})
+const router = Router({ prefix: '/api/v1' })
 
 router.get('/', publicAPI)
 router.get('/private', auth, privateAPI)
 
-function publicAPI(ctx) {
-  ctx.body = {message:'This is the public page'}
+function publicAPI (ctx) {
+  ctx.body = { message: 'This is the public page' }
 }
 
-function privateAPI(ctx) {
+function privateAPI (ctx) {
   const user = ctx.state.user
+  ctx.status = 200;
   ctx.body = {
-    message:`Hello ${user.username}, this is the private page`, user
+    message: `Hello ${user.username}, you have logged in succesfully.`, user
   }
+  
 }
 
-
-
-module.exports = router;
+module.exports = router
